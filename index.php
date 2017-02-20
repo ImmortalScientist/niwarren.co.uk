8081c2691da5e5ffd10f62323196f4b9265d5438
ImmortalScientist <mail.nick1282@gmail.com>
2017-02-20 22:03:48 +0000

    added uptime script

--- index.php
+++ index.php
@@ -46,9 +46,25 @@
             <section>
                 This is my personal website - on it you can find information about projects which I have delved into, and .<br><br>
                 I am a student at the <a href="http://www.reading.ac.uk/">University of Reading</a> studying Electronic Engineering (BEng).<br>
-                In my spare time, I like tinkering with RC Multirotors, playing the French Horn and I'm also a fairly keen Archer!
+                In my spare time, I like tinkering with RC Multirotors, playing the French Horn and I'm also a fairly keen Archer!<br>
+                <?php
+                  exec("uptime", $system); // get the uptime stats
+                  $string = $system[0]; // this might not be necessary
+                  $uptime = explode(" ", $string); // break up the stats into an array
+
+                  $up_days = $uptime[4]; // grab the days from the array
+
+                  $hours = explode(":", $uptime[7]); // split up the hour:min in the stats
+
+                  $up_hours = $hours[0]; // grab the hours
+                  $mins = $hours[1]; // get the mins
+                  $up_mins = str_replace(",", "", $mins); // strip the comma from the mins
+
+                  echo "The server has been up for " . $up_days . " days, " . $up_hours . " hours, and " . $up_mins . " minutes.";// echo the results
+                ?>
+                <br>
             </section>
-          <hr>
+
             <header>
               <h3>Links</h3>
             </header>
