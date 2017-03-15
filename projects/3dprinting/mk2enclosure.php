e1ded7193e0ca50018407803bb4897e2f7a094fb
ImmortalScientist <mail.nick1282@gmail.com>
2017-03-15 18:01:10 +0000

    titles edited

--- projects/3dprinting/mk2enclosure.php
+++ projects/3dprinting/mk2enclosure.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Nick Warren</title>
+        <title>Nick Warren | Projects | Prusa i3 MK2 Enclosure</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- projects/rc/quad280.php
+++ projects/rc/quad280.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Nick Warren</title>
+        <title>Nick Warren | Projects | 280mm Mini Quadcopter</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
